94b6c82163c1d00bbc233ef430770963916b3aa7
David Martín Pérez <davidmartinperez1@gmail.com>
2019-07-18 20:40:31 +0200

    mvp

---
+++ precache-manifest.a5aa45e3c393a0dd6e3e14a799e69115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bdb4ea92a622e05ff72",
+    "url": "/static/js/main.5bdb4ea9.chunk.js"
+  },
+  {
+    "revision": "319eb2e256de426da6c0",
+    "url": "/static/js/1.319eb2e2.chunk.js"
+  },
+  {
+    "revision": "5bdb4ea92a622e05ff72",
+    "url": "/static/css/main.e669291d.chunk.css"
+  },
+  {
+    "revision": "1255288247522fe479793ef69a1fdbb3",
+    "url": "/index.html"
+  }
+];
